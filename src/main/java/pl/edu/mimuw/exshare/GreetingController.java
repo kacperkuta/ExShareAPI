8ab3fbfc4d28f1da8f2c774f4c4cde2d047f1daa
kacperkuta <kacperkuta@gmail.com>
2020-04-04 20:02:10 +0200

    Druga próba

---
+++ src/main/java/pl/edu/mimuw/exshare/GreetingController.java
@@ -0,0 +1,29 @@
+package pl.edu.mimuw.exshare;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting2")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("/request")
+    public Greeting greeting2(@RequestParam(value = "name", defaultValue = "Świecie") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("/request2")
+    public Greeting greeting3(@RequestParam(value = "name", defaultValue = "Świecie") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
