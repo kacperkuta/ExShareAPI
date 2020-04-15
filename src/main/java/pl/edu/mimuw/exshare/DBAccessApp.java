fb1d3601a67177d6bd48f3a406fb6907ab9c83d1
kacperkuta <kacperkuta@gmail.com>
2020-04-15 18:27:42 +0200

    DBControl added

--- src/main/java/pl/edu/mimuw/exshare/DBAccessApp.java
+++ src/main/java/pl/edu/mimuw/exshare/DBAccessApp.java
@@ -1,11 +1,17 @@
 package pl.edu.mimuw.exshare;
+import java.util.Arrays;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DBAccessApp {
-    public static void main (String[] args) {
-        SpringApplication.run(DBAccess.class, args);
+
+    public static void main(String[] args) {
+        SpringApplication.run(DBAccessApp.class, args);
     }
+
 }
