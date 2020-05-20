3a322932d236c277f357d042c21130c628366761
kacperkuta <kacperkuta@gmail.com>
2020-05-20 13:55:47 +0200

    Spring database

--- src/main/java/pl/edu/mimuw/exshare/GeneralController.java
+++ src/main/java/pl/edu/mimuw/exshare/GeneralController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.edu.mimuw.exshare.AssignementsController.AssignmentsRepository;
+import pl.edu.mimuw.exshare.CourseTestsController.CourseTestsRepository;
 import pl.edu.mimuw.exshare.CoursesController.CoursesRepository;
+import pl.edu.mimuw.exshare.TestTasksController.TestTasksRepository;
 import pl.edu.mimuw.exshare.UsersController.UsersRepository;
 
 @RestController
@@ -18,10 +20,18 @@ public class GeneralController {
     @Autowired
     AssignmentsRepository assignmentsRepository;
 
+    @Autowired
+    TestTasksRepository testTasksRepository;
+
+    @Autowired
+    CourseTestsRepository courseTestsRepository;
+
     @GetMapping("/clearAll")
     public void clearAll() {
         usersRepository.deleteAll();
         coursesRepository.deleteAll();
         assignmentsRepository.deleteAll();
+        testTasksRepository.deleteAll();
+        courseTestsRepository.deleteAll();
     }
 }
